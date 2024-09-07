a95a9c4e3e224f86de6a8b7ffd33b41e5224038b
Touhidul Islam <toouhidul@gmail.com>
2024-09-07 13:09:20 +0600

    Custom User Model, Custom Token Generation, custom authentication with RSA key generation

---
+++ myapp/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('register/', views.RegisterView.as_view()),
+    path('login/', views.LoginView.as_view()),
+    path('info/', views.UserInfoView.as_view()),
+]
